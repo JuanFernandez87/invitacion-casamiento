2d08ce899da76c1b3fcf4a4812f2d8cf63ce4560
Juan Fernandez <juanfernandez_carc@hotmail.com>
2022-01-03 11:04:57 -0300

    Correcciones y modificaciones en estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { TimerComponent } from './timer/timer.component';
 import { RegaloComponent } from './regalo/regalo.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselComponent } from './carousel/carousel.component'
+import { AlojamientosComponent } from './alojamientos/alojamientos.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component'
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavbarComponent,
     TimerComponent,
     RegaloComponent,
-    FooterComponent
+    FooterComponent,
+    CarouselComponent,
+    AlojamientosComponent,
+    UbicacionComponent
   ],
   imports: [
     BrowserModule,
